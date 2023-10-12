fc32c1b73b4c7757b8250e2c34c0a1de3a01ba43
Sri <sriar.srivati@ansira.com>
2023-10-12 11:44:31 -0700

    form initial

--- js/min.js
+++ js/min.js
@@ -2,4 +2,22 @@ $(document).ready(function () {
   $(".nav_menu").click(function () {
     $(".navbar").toggleClass("navbar_mob");
   });
+
+  /*********** Google form ***********/
+  $("#ss-form").submit(function (e) {
+    $("#ss-form")[0].reset();
+    e.preventDefault();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdC97FvuSDsijxYxEXBIgbUnappLqK46XJL2bsPqr44tEEgRA/formResponse",
+      data: $(this).serialize(),
+      type: "POST",
+      dataType: "xml",
+      success: function (data) {
+        console.log("Submission successful");
+      },
+      error: function (xhr, status, error) {
+        console.log("Submission failed: " + error);
+      },
+    });
+  });
 });
